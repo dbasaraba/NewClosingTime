958e6c12a468a01f6e95d7cbd548feaee9c506bf
slaam001 <71736989+slaam001@users.noreply.github.com>
2020-11-11 13:56:31 -0600

    Added Seller and Offer to relational
    Added Seller and Offer

---
+++ src/main/java/edu/depaul/se452/group9/ClosingTime/relational/Seller.java
@@ -0,0 +1,94 @@
+package edu.depaul.se452.group9.ClosingTime.relational;
+import java.util.List;
+import java.util.UUID;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import org.bson.codecs.pojo.annotations.BsonId;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@Entity
+public class Seller
+{
+        @Id
+        @GeneratedValue
+        private String sellerID;
+        private String firstName;
+        private String lastName;
+        private String Email;
+
+        public Seller(String firstName, String lastName, String Email)
+        {
+
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.Email = Email;
+        }
+
+        @Override
+        public String toString(){
+            return String.format("Seller[sellerID='%s', firstName='%s', lastName='%s', Email='%s']",
+                    sellerID,firstName,lastName,Email);
+        }
+        /**
+         * @return int return the sellerID
+         */
+        public String getSellerID() {
+            return sellerID;
+        }
+
+        /**
+         * @param sellerID the sellerID to set
+         */
+        public void setSellerID(String sellerID) {
+            this.sellerID = sellerID;
+        }
+
+        /**
+         * @return String return the firstName
+         */
+        public String getFirstName() {
+            return firstName;
+        }
+
+        /**
+         * @param firstName the firstName to set
+         */
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+
+        /**
+         * @return String return the lastName
+         */
+        public String getLastName() {
+            return lastName;
+        }
+
+        /**
+         * @param lastName the lastName to set
+         */
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
+
+        /**
+         * @return String return the Email
+         */
+        public String getEmail() {
+            return Email;
+        }
+
+        /**
+         * @param Email the Email to set
+         */
+        public void setEmail(String Email) {
+            this.Email = Email;
+        }
+    }
+
+
